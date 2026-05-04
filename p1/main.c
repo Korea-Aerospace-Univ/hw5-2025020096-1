7f7791688f1c7e0b11c403a6e45de904ab1d1700
2025020096 <a58865486@gmail.com>
2026-05-04 15:47:39 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    bool found = false;
+
+    scanf("%d", &n);
+
+    for (int a = 1; a * 900 <= n; a++) {
+        for (int b = 2; b * 750 <= n; b += 2) { 
+            for (int c = 1; c * 200 <= n; c++) {
+                
+                
+                if ((a * 900 + b * 750 + c * 200) == n) {
+                    
+                    
+                    if (c < a || c < b) {
+                        printf("%d %d %d\n", a, b, c);
+                        found = true; 
+                    }
+                }
+            }
+        }
+    }
+
+    if (!found) { 
+        printf("none\n");
+    }
+    return 0;
+    }
